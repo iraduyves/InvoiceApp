07c788cd2a5eed9ef4e9d0cbef7e57414bb875f5
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-10-06 09:02:30 +0200

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Router, Routes } from "react-router-dom"
+import Siderbar from "./Components/SideBar/SideBar"
+import Home from "./Home"
+import "./App.css"
+import Invoices from "./Invoices"
+
+function App() {
+  return (
+   <main>
+    <Siderbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/invoice" element={<Invoices/>} />
+       {/* <Route path="/AddNew" element={<AddNew/>}/> */}
+      <Route path="*" element={<div>NOt Found</div>} />
+     
+    </Routes>
+    
+    
+   </main>
+  )
+}
+
+export default App
